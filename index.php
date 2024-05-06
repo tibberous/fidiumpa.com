ce0c2f92fc5a6247858f0a74b13497baf627b896
Trent Tompkins <TrentTompkins@gmail.com>
2024-05-06 13:31:31 -0400

    Changed title

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Xeon | OnePage Responsive Theme</title>
+    <title>Fidium Fiber Optic :: Pennsylvania</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
     <link href="css/prettyPhoto.css" rel="stylesheet">
