eb8d5d539c5a57ef1a8836d95ab96f34ae62d33f
Trent Tompkins <TrentTompkins@gmail.com>
2024-05-06 12:35:05 -0400

    added remote testing code

--- includes/database.php
+++ includes/database.php
@@ -1,12 +1,16 @@
 <?php
 
+    
+
     if($_SERVER['DOCUMENT_ROOT'] === "/home/u678805949/domains/fidiumpa.com/public_html"){
         // use database settings for Hostinger
         $password = file_get_contents("hostinger.key");
 
-        $mysqli = new mysqli("localhost:3306", $user="root", $password, "test");
+        //$mysqli = new mysqli("localhost:3306", $user="root", $password, "test");
 
         echo "Live Server";
+        echo "Password:50{$password}";
+        die();
 
         if /*($mysqli->connect_errno) {
             printf("Connect failed: %s\n", $mysqli->connect_error);
