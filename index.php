55b1721baf531e8f1ee58baaee13a5beed59348d
Trent Tompkins <TrentTompkins@gmail.com>
2024-05-06 13:46:47 -0400

    add anaylitics

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C8Y6X9EJJC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-C8Y6X9EJJC');
+</script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
