4017827e5ce8aa9525c94e92b93cc9ac3ef45e3b
Trent Tompkins <TrentTompkins@gmail.com>
2024-05-06 13:25:52 -0400

    fix database connection

--- includes/database.php
+++ includes/database.php
@@ -2,28 +2,35 @@
 
     
 
-    if($_SERVER['DOCUMENT_ROOT'] === "/home/u678805949/domains/fidiumpa.com/public_html"){
+if($_SERVER['DOCUMENT_ROOT'] === "/home/u678805949/domains/fidiumpa.com/public_html"){
         // use database settings for Hostinger
         $password = file_get_contents("hostinger.key");
 
-        $mysqli = new mysqli("156.67.73.101:3306", $user="u678805949_fidium", $password, "test");
+        $mysqli = new mysqli("localhost", $user="u678805949_fidiumpa", "{$password}50", "u678805949_fidiumpa");
 
 
         if ($mysqli->connect_errno) {
             printf("Connect failed: %s\n", $mysqli->connect_error);
             exit();
-        }
-    } else {
+        } else {
+        echo "Connected!";
+        die();
+    }
+
+} else {
         $password = file_get_contents("localhost.key");
 
-        $mysqli = new mysqli("localhost:3306", $user="root", $password, "test");
+        $mysqli = new mysqli("156.67.73.101:3306", $user="u678805949_fidiumpa", "{$password}50", "u678805949_fidiumpa");
 
         if ($mysqli->connect_errno) {
             printf("Connect failed: %s\n", $mysqli->connect_error);
             exit();
         }
 
-        //print_r($mysqli);
+        print_r($mysqli);
+
+        echo "Connected";
+        die();
     }
 
 
