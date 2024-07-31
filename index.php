24a6c133de4caaf8181c86a369caad7edad2723e
Trent Tompkins <TrentTompkins@gmail.com>
2024-07-30 20:16:45 -0400

    add temporary code to get document dimensions

--- index.php
+++ index.php
@@ -1,10 +1,56 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <title>Fidium Fiber Optic Internet in Pennsylvania</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @media screen {
+            p::first-line {
+                font-size: 200%;
 
+            }
+
+        }
+
+    </style>
+    
 </head>
 <body>
 
+    <div class="wrapper" style="border: 1px red solid">
+
+        <section>
+
+            <div style="width: 50%; border: 1px green solid">
+                <p>
+                    Here is my<br>
+                    text.
+                </p>
+
+            </div>
+            <div style="width: 50%; border: 1px blue solid">
+Y
+
+            </div>
+
+        </section>
 
 
+    </div>
+
+
+    <script src="js/jquery.js"></script>
+    <script>
+        $(window).resize(function(){
+            $(".wrapper").html("Width: "+$(document).width()+"<br>Heigth: "+$(document).height());
+        });
+
+        $(function() {
+
+            //$(".wrapper").html("Width: "+$(document).width());
+            
+        });
+    </script>
 </body>
 </html>
